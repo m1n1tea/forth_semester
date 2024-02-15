ba62db26001c27cd04f23d6af74edca86cb3fbc0
dueip <68518940+dueip@users.noreply.github.com>
2024-02-16 01:36:19 +0500

    Started doing command history

--- CalculatorUi.py
+++ CalculatorUi.py
@@ -16,8 +16,28 @@ class Command:
         return self.number
     
     number : int = 0
-    commandLine:QLabel = None
+    commandLine: QLabel = None
+    
 
+class CommandHistory: 
+    def __init__(self, commandLine: QLabel):
+        self.commandLine = commandLine
+
+    def push(self, command: Command):
+        self.commands.append(command)
+    
+    def top(self) -> Command:
+        return self.commands[-1]
+    
+    # Returns the last command used
+    def pop(self) -> Command:
+        last_command_used: Command = self.top()
+        self.commands.pop()
+        return last_command_used
+
+
+    commands: list[Command]
+    commandLine: QLabel = None
 
 
 app = QApplication([])
@@ -32,10 +52,12 @@ form.setupUi(window)
 
 
 buttonLayout : QGridLayout = form.buttonLayout
-current_command: Command = Command(buttonLayout.itemAtPosition(0, 1).widget())
+
+#current_command: Command = Command(buttonLayout.itemAtPosition(0, 1).widget())
+
 
 for row_index in range(buttonLayout.rowCount()): 
-    for column_index in range(buttonLayout.columnCount()):
+    for column_index in range(buttonLayout.columnCount() + 3):
         if buttonLayout.itemAtPosition(column_index, row_index) is not None:  
             button = buttonLayout.itemAtPosition(column_index, row_index).widget()
             if isinstance(button, QPushButton):
